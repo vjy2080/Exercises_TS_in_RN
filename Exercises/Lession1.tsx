5169502d63fdf9aecb201a8f9a7967ffea010b59
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2024-01-09 19:26:00 +0530

    Update

---
+++ Exercises/Lession1.tsx
@@ -0,0 +1,29 @@
+// Website for this exercises -->  https://typescript-exercises.github.io/#exercise=1&file=%2Findex.ts
+
+// Given the data, define the interface "User" and use it accordingly.
+
+export interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
